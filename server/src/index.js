9006be228adf799e9df00b0b753544d4c9217c21
kwiecien <k.d.kwiecinski@gmail.com>
2020-11-21 23:58:03 +0100

    implement resolver functions

--- server/src/index.js
+++ server/src/index.js
@@ -13,10 +13,22 @@ type Link {
 }
 `
 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+}]
+
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
-    }
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Link: {
+        id: parent => parent.id,
+        description: parent => parent.description,
+        url: parent => parent.url,
+    },
 }
 
 const server = new GraphQLServer({
