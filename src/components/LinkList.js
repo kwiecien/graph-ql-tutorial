46c589cb5ba1cf8c1b21e42fd110a8265752f26f
kwiecien <k.d.kwiecinski@gmail.com>
2020-11-22 16:01:32 +0100

    fetch and render feed

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,24 +1,28 @@
 import React, {Component} from "react";
 import Link from "./Link";
+import gql from 'graphql-tag'
+import {Query} from 'react-apollo'
+
+const FEED_QUERY = gql`
+    {
+        feed {
+            id
+            url
+            description
+        }
+    }
+`
 
 class LinkList extends Component {
     render() {
-        const linksToRender = [
-            {
-                id: '1',
-                description: 'Prisma turns your database into a GraphQL API 😎',
-                url: 'https://www.prismagraphql.com',
-            },
-            {
-                id: '2',
-                description: 'The best GraphQL client',
-                url: 'https://www.apollographql.com/docs/react/',
-            },
-        ]
-
-        return <div>
-            {linksToRender.map(link => <Link key={link.id} link={link}/>)}
-        </div>
+        return <Query query={FEED_QUERY}>
+            {({loading, error, data}) => {
+                if (loading) return <div>Fetching</div>
+                if (error) return <div>Error</div>
+                const {feed} = data;
+                return feed.map(link => <Link key={link.id} link={link}/>)
+            }}
+        </Query>
     }
 }
 
