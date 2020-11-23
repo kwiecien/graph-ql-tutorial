62854c164e53d700cd260095b7b2e05851aaa9e4
kwiecien <k.d.kwiecinski@gmail.com>
2020-11-23 17:01:21 +0100

    use resolvers

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,15 @@
-const {deleteLink, updateLink, post} = require("../../src/resolvers/Mutation");
-const {link, feed, info} = require("../../src/resolvers/Query");
+const Mutation = require("../../src/resolvers/Mutation");
+const Query = require("../../src/resolvers/Query");
+const User = require("../../src/resolvers/User");
+const Link = require("../../src/resolvers/Link");
 const {PrismaClient} = require('@prisma/client')
 const {GraphQLServer} = require('graphql-yoga')
 
 const resolvers = {
-    Query: {
-        info,
-        feed,
-        link
-    },
-    Mutation: {
-        post,
-        updateLink,
-        deleteLink,
-    }
+    Query,
+    Mutation,
+    User,
+    Link,
 }
 
 const prisma = new PrismaClient()
